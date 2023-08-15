cebd28753d4a8d3ba011a3f0fb9af52a57ae6b43
delus <duybui@maxflowtech.com>
2023-08-15 11:26:51 +0700

    add next js backend

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,115 +1,117 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { obstacles } from "../public/obstacles";
+import CarIcon from "../public/car/carIcon";
+import React from "react";
+
+const gridSize = 500;
+const gridCount = 50; // No. of squares in each direction
+const squareSize = gridSize / gridCount;
+
+const coordsToObstacles = [];
+obstacles.forEach(([xStart, xEnd, yStart, yEnd, color]) => {
+  let x = xStart;
+  while (x <= xEnd) {
+    let y = yStart;
+    while (y <= yEnd) {
+      coordsToObstacles[`${x}:${y}`] = color || "#c1c3c7";
+      y += 1;
+    }
+    x += 1;
+  }
+});
+
+const Obstacle = ({ x, y, color }) => (
+  <rect
+    width={squareSize}
+    height={squareSize}
+    x={x}
+    y={y}
+    fill={color}
+    stroke={color}
+  />
+);
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: props.next,
+      rotation: props.rotation,
+    };
+  }
+
+  render() {
+    const { position, rotation } = this.state;
+    const [x, y] = position;
+    return (
+      <CarIcon
+        x={x * squareSize - 20}
+        y={y * squareSize - 20}
+        rotation={rotation}
+      />
+    );
+  }
+}
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: [],
+    };
+  }
+
+  async placeCars() {
+    this.setState(() => ({
+      cars: [
+        {
+          id: "car1",
+          next: [16, 14],
+          rotation: 0,
+        },
+        {
+          id: "car2",
+          next: [26, 16],
+          rotation: 270,
+        },
+      ],
+    }));
+  }
+
+  componentDidMount() {
+    this.placeCars();
+  }
+
+  render() {
+    const obstacleElems = [];
+    for (let [key, color] of Object.entries(coordsToObstacles)) {
+      const [x, y] = key.split(":");
+      obstacleElems.push(
+        <Obstacle
+          key={`${x}:${y}`}
+          x={x * squareSize}
+          y={y * squareSize}
+          color={color}
+        />
+      );
+    }
+
+    const cars = this.state.cars.map(({ id, next, rotation }) => {
+      return <Car key={id} next={next} rotation={rotation} />;
+    });
+
+    return (
+      <svg width={gridSize} height={gridSize} className="map">
+        {obstacleElems}
+        {cars}
+      </svg>
+    );
+  }
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div className="App">
+      <Map />
     </div>
-  )
+  );
 }
