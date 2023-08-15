c5535fbdba75da29d63c2b4082fe048058c41fd4
delus <duybui@maxflowtech.com>
2023-08-15 16:39:01 +0700

    add revision for rides table

---
+++ backend/migrations/versions/fe0ea0687891_add_rides_table.py
@@ -0,0 +1,30 @@
+"""add rides table
+
+Revision ID: fe0ea0687891
+Revises: 5d5f5b1ecf55
+Create Date: 2023-08-15 16:36:11.348012
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fe0ea0687891'
+down_revision = '5d5f5b1ecf55'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "rides",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("car_id", sa.Integer(), nullable=False, unique=True),
+        sa.Column("location", sa.String(length=255), nullable=False),
+        sa.Column("path", sa.Text(), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("rides")
