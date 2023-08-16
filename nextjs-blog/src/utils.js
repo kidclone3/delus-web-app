2e32ee0e3ebb32c4288e3f09205a48d57500fff7
kidclone3 <kid.clone.3@gmail.com>
2023-08-16 22:13:48 +0700

    update frontend for get location data from api

--- nextjs-blog/src/utils.js
+++ nextjs-blog/src/utils.js
@@ -9,3 +9,22 @@ export const wait = (t) =>
       res();
     }, t);
   });
+
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const baseURL =
+  process.env.APP_ENV === "dev"
+    ? "http://localhost:8000"
+    : "https://delusthefirst.tech";
+
+export const api = {};
+api.get = async (endpoint) => {
+  const res = await fetch(`${baseURL}${endpoint}`);
+  if (res.json) return await res.json();
+  return res;
+};
