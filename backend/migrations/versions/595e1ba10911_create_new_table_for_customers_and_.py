93ba8b45e5c31b9a60b61c22f89198fb7145d131
kidclone3 <kid.clone.3@gmail.com>
2023-08-23 18:35:27 +0700

    modify alembic version

--- backend/migrations/versions/595e1ba10911_create_new_table_for_customers_and_.py
+++ backend/migrations/versions/595e1ba10911_create_new_table_for_customers_and_.py
@@ -24,24 +24,24 @@ def upgrade() -> None:
     op.create_table(
         "customers",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column("customer_id", sa.BINARY(length=16), unique=True),
+        sa.Column("customer_id", sa.String(36), unique=True),
         sa.Column("name", sa.String(255), nullable=False, unique=True),
         sa.Column("active", sa.Boolean, default=True),
         sa.Column("location", sa.String(5), nullable=False),
         sa.Column("destination", sa.String(5), nullable=True),
-        sa.Column("driver_id", sa.BINARY(length=16), unique=True)
+        sa.Column("driver_id", sa.String(36), unique=True)
     )
 
     # create table drivers
     op.create_table("drivers",
         sa.Column("id", sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column("driver_id", sa.BINARY(length=16), unique=True),
+        sa.Column("driver_id", sa.String(36), unique=True),
         sa.Column("name", sa.String(255), nullable=False, unique=True),
         sa.Column("status", sa.Integer, default=True),
         sa.Column("location", sa.String(5), nullable=False),
         sa.Column("path", sa.Text(), nullable=True),
         sa.Column("path_index", sa.Integer, nullable=True),
-        sa.Column("customer_id", sa.BINARY(length=16), unique=True)
+        sa.Column("customer_id", sa.String(36), unique=True)
     )
 
 
